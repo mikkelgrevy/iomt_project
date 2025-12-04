6383840ac7be8536c9993ce18b709e940dac087d
Mikkel Grevy <mmgrevy@gmail.com>
2025-12-04 14:19:05 +0100

    Updated for added features in backend

--- embedded/main.py
+++ embedded/main.py
@@ -1,1 +1,8 @@
-#
+import urequests as requests
+import time
+
+while True:
+    if wifi.is_connected()
+    res = requests.get(url='http://127.0.0.1:8000/dispenser/config/1)
+    print(res.json)
+    time.sleep(3)
